f5f49a844b78c30eef867b0be82ae5b7fcafbc66
Barnabas Nduwak Suku <barnssuku@gmail.com>
2018-03-09 14:44:18 +0100

    gotten the project list actions right

---
+++ web/js/on-off-display-div.js
@@ -0,0 +1,29 @@
+/**
+ * This script will be use to toggle on-off the content of the listed projects 
+ * on a click to any of the list tab.
+ */
+$(document).ready(function(){
+	$('.my-list').on('click', function(){		
+		var id = this.id;
+		// call the method to do the removal of all show classes
+		//console.log($('div.display-div'));
+		remShow($('div.display-div'));
+		// after then we call the hide class on
+		$('.item_'+id).addClass('show');		
+	});
+	
+	/**
+	 * This function will take any element and set its 
+	 * class display attribute to 'hide' if it is having
+	 * 'show' and vice versa. 
+	 */
+	var remShow = function($elem) {
+		// At this point $elem is an object
+		$elem.each(function() {			
+			$(this).removeClass('show');
+			$(this).addClass('hide');
+		});
+	}	
+	
+	
+});
